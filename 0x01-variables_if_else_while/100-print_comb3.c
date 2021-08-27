642741fdfd939e92630297bd024cecedcff1794e
mrnpro <yegetanaty03@gmail.com>
2021-08-27 18:08:23 -0500

    C

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,12 +3,20 @@ int main(void)
 {
 for(int i=0; i<=8; i++)
 {
-	for(int j=1;j<=9;j++)
-		{
-		if(i==8&&j==9)
-		printf("%d%d",i,j);
-		else
- 		printf("%d%d, ",i,j);
+	for (int j=1;j<=9;j++)
+	{
+			if (i==8&&j==9)
+			{
+				putchar(j);
+				putchar(i);
+				putchar(',');
+				putchar(' ');
+			}
+			else
+ 			{	putchar(i);
+				putchar(j);
+				putchar(' ');
+			}
 		}
 }
 
