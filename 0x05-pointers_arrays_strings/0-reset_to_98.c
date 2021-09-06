6facbcf42365d92860952ed62c2e2470c14c7930
mrnpro <yegetanaty03@gmail.com>
2021-09-06 11:00:28 -0500

    change the value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
 *reset_to_98 - changes to 98
 *
-*n: is the number passed by the main
+*@n: is the number passed by the main
 */
 
 void reset_to_98(int *n)
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,8 @@
+#include "holberton.h"
+/**
+*_strlen - this tells us 
+*@s:
+int _strlen(char *s)
+{
+strlen(*s);
+}
