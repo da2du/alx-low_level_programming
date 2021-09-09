9d69ee357cb2d69af005db6e629e853ffa38147c
mrnpro <yegetanaty03@gmail.com>
2021-09-09 04:47:01 -0500

    the strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,13 @@
 #include "holberton.h"
 #include <string>
 /**
+*_strcat - concatinates two strings and store it in dest
+*@dest: the destination
+*@src: the source
+*Return:the dest value
 */
 char *_strcat(char *dest, char *src)
 {
-strcat(dest,src);
+strcat(dest, src);
 return (dest);
 }
