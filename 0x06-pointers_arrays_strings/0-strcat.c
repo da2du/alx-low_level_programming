979db716eba7584aa6b3eac2ff8042ed24af69a8
mrnpro <yegetanaty03@gmail.com>
2021-09-09 04:56:41 -0500

    tell the length

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,26 @@
 #include "holberton.h"
-#include <string>
 /**
-*_strcat - concatinates two strings and store it in dest
-*@dest: the destination
-*@src: the source
-*Return:the dest value
-*/
+ * _strcat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to dest.
+ */
 char *_strcat(char *dest, char *src)
 {
-strcat(dest, src);
-return (dest);
+	int count = 0, count2 = 0;
+
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count2 >= 0)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
 }
