ad0b86d291d07e49feb48f77a39786b9a548945f
mrnpro <yegetanaty03@gmail.com>
2021-09-15 10:14:56 -0500

    this is new edit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
-/* _puts_recursion -  provides the passed string from main
-*&s:the character from main
+/**
+*_puts_recursion - provides the passed string from main
+*@s : the character from main
 */
 void _puts_recursion(char *s)
 {
